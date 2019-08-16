646dd326c1978a30e09f78c29a5257b3864790fa
Nicolás Yeomans <53536358+nmyeomans@users.noreply.github.com>
2019-08-15 21:39:14 -0400

    Add files via upload

---
+++ Difusión de calor 2D.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 13 16:09:50 2019
+
+@author: nico7
+"""
+
+import scipy as sp
+from matplotlib.pyplot import*
+import numpy as np
+
+
+#difusion 1D
+#Dominio(omega)  x E(0,L=1,0)
+#du/dt - k*d^2u/dt^2*cp = q(t)
+# u(0,t=0)
+#u(l,t)=20
+#u(x,0)=u0(x)=0
+# diferencias finitas(algoritmo)
+#dominio(omega) --> aproximaremos  x=0 , x=dx , xn=L tendremos:
+# n+1 puntos y n intervalos , con esto dx=L/n, cada uno se asocia un u de temperatura
+# estructura de datos se representa por un vector de numeros reales con un arreglo de float32 con n+1
+a=1.
+b=1.
+Nx=6
+Ny=6
+
+dx = a /Nx
+dy = b / Ny
+
+h = dx
+
+if dx != dy:
+    print "Error!!! dx != dy"
+    exit(-1) # -1 le dice al SO que el programa fallo
+
+#Funcioo de conveniencia para calcular coordenadas del punto (i,j)
+
+#def coord(i,j):
+#    return dx*i, dy*j
+
+#i, j = 4, 2
+#x, y = dx*i, dy*j
+    
+coord = lambda i,j : (dx*i, dy*j)
+x,y =  coord(4,2)
+
+print "x = ", x
+print "y = ", y
+
+u_k = np.zeros((Nx+1, Ny+1), dtype = np.double) # dtype es para el tipo de datos(int, float, float32, float64, double, etc)
+u_km1 = np.zeros((Nx+1, Ny+1), dtype = np.double)
+
+#CB escencial
+#u_k[Ny,:] = 20
+#u_k[:,0]=20
+
+#buena idea definir funciones que hagan el codigo expresivo
+def printbien(u):
+    print u.T[Nx::-1,:]
+    
+u_k[0,:] = 20
+u_k[:,0]=20
+
+def imshowbien(u):
+    imshow(u.T[Nx::-1,:])
+    
+print u_k
+printbien(u_k)
+figure()
+imshowbien(u_k)
+show()
+
+#parametros del problema
+dt=1    #s
+K=79.5  #m^2/s
+c=450.  #J/KgC
+rho=7800.   #Kg/m^3
+
+alpha=K*dt/(c*rho*dx**2)
+print "dt=",dt
+print "dx=",dx
+print"K=",K
+print "c=",c
+print "rho=",rho 
+print "alpha=", alpha
+
+
+
+k=0
+figure(1)
+imshowbien(u_k)
+title("k= {}  t= {} s".format(k,k*dt))
+
+#savefig("movie/frame_ {0:04.0f}.png".format(k))
+close(1)
+
+for k in range (1,100):
+    t=dt*(k+1)
+    
+    #loop en el espacio i=1....n   u_km1[0]=0 u_km1[n]=20
+    u_k[0,:] = 20
+    u_k[:,0]=20
+    
+    for i in range(1,Nx-1):
+        for j in range(1, Ny-1):
+            #algoritmo de dif finitas
+            nabla_u_k = (u_k[i-1,j]+ u_k[i+1,j]+ u_k[i, j-1] + u_k[i,j+1] - 4*u_k[i,j])/h**2
+            u_km1[i,j] = u_k[i,j] + alpha*nabla_u_k
+   
+    # avanzar la solucion a k+11
+    u_k=u_km1
+    
+    #CB natural
+    u_km1[Nx,:] = u_km1[Nx,:]   
+    u_km1[:, Ny] = u_km1[:,Ny]
+   
+    figure(1)
+    imshowbien(u_k)
+    title("k= {}  t= {} s".format(k,k*dt))
+#    savefig("movie/frame_{0:04.0f}.png".format(k))
+
+
+#
+#figure(2)
+#imshowbien(u_k)
+#title("k= {}  t= {} s".format(k,k*dt))
+#
+show()
+
+
+
